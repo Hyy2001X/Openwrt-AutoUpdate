cd0245949b01c528383de2335d0f4518875ff364
Hyy2001X <1804430051@qq.com>
2020-08-27 19:17:15 +0800

    V1.6

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 # AutoBuild Script Module by Hyy2001
+# AutoUpdate
 
-Version=V1.5
-Github_Host_URL=https://github.com/Hyy2001X/Openwrt-AutoUpdate
-Github_Release_URL=$Github_Host_URL/releases/tag/AutoUpdate
-Github_Download_URL=$Github_Host_URL/releases/download/AutoUpdate
-if [ ! -f /etc/openwrt_date ];then
-	echo "本地固件版本信息获取失败!"
-	exit
-fi
+Version=V1.6
+Github=https://github.com/Hyy2001X/Openwrt-AutoUpdate
+Github_Release=$Github/releases/tag/AutoUpdate
+Github_Download=$Github/releases/download/AutoUpdate
 clear
 echo -e "Auto-Update Script $Version by Hyy2001\n"
+if [ ! -f /etc/openwrt_date ];then
+	echo "暂不兼容当前固件!"
+	exit
+fi
 CURRENT_VERSION=`cat /etc/openwrt_date`
 if [ "$CURRENT_VERSION" == "" ]; then
-	echo "本地固件版本信息获取失败!"
+	echo "当前固件信息获取失败!"
 	exit
 fi
 cd /tmp
 echo "开始获取云端版本信息..."
-GET_Version=`wget --no-check-certificate -q $Github_Release_URL -O - | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+' | awk 'NR==1'`
+GET_Version=`wget --no-check-certificate -q $Github_Release -O - | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+' | awk 'NR==1'`
 if [ "$GET_Version" == "" ]; then
-	echo "云端版本获取失败!"
+	echo "云端版本信息获取失败!"
 	exit
 fi
 echo -e "\n当前版本: $CURRENT_VERSION"
 echo "云端版本: $GET_Version"
 if [ $CURRENT_VERSION == $GET_Version ];then
-	echo -e "\n当前已是最新版本,无需更新!"
+	echo -e "\n已是最新版本,无需更新!"
 	exit
 fi
 Firmware=AutoBuild-d-team_newifi-d2-Lede-$GET_Version.bin
 Firmware_Detail=AutoBuild-d-team_newifi-d2-Lede-$GET_Version.detail
 echo "云端固件名称:$Firmware"
 echo -e "\n开始下载云端固件..."
-wget --no-check-certificate -q $Github_Download_URL/$Firmware -O $Firmware
+wget --no-check-certificate -q $Github_Download/$Firmware -O $Firmware
 if [ ! "$?" == 0 ]; then
 	echo "[$Firmware]下载失败!"
 	exit
@@ -41,7 +42,7 @@ fi
 echo "...下载成功!"
 echo "固件大小:$(du -h $Firmware | awk '{print $1}')"
 echo -e "\n开始下载固件详细信息..."
-wget --no-check-certificate -q $Github_Download_URL/$Firmware_Detail -O $Firmware_Detail
+wget --no-check-certificate -q $Github_Download/$Firmware_Detail -O $Firmware_Detail
 if [ ! "$?" == 0 ]; then
 	echo "[$Firmware_Detail]下载失败!"
 	exit
