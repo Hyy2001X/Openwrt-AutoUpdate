1ab60cbe79ce10c7a54713cb4322c6447487f44d
Hyy2001X <1804430051@qq.com>
2020-08-27 18:52:38 +0800

    V1.5

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # AutoBuild Script Module by Hyy2001
 
-Version=V1.4
+Version=V1.5
 Github_Host_URL=https://github.com/Hyy2001X/Openwrt-AutoUpdate
 Github_Release_URL=$Github_Host_URL/releases/tag/AutoUpdate
 Github_Download_URL=$Github_Host_URL/releases/download/AutoUpdate
@@ -34,11 +34,11 @@ Firmware_Detail=AutoBuild-d-team_newifi-d2-Lede-$GET_Version.detail
 echo "云端固件名称:$Firmware"
 echo -e "\n开始下载云端固件..."
 wget --no-check-certificate -q $Github_Download_URL/$Firmware -O $Firmware
-if [ ! -f  $Firmware ]; then
+if [ ! "$?" == 0 ]; then
 	echo "[$Firmware]下载失败!"
 	exit
 fi
-echo "...下载成功,固件名称:[$Firmware]"
+echo "...下载成功!"
 echo "固件大小:$(du -h $Firmware | awk '{print $1}')"
 echo -e "\n开始下载固件详细信息..."
 wget --no-check-certificate -q $Github_Download_URL/$Firmware_Detail -O $Firmware_Detail
@@ -49,14 +49,17 @@ fi
 echo "...下载成功!"
 GET_SHA=`awk -F'[ :]' '/SHA256/ {print $2;exit}' $Firmware_Detail`
 CURRENT_SHA=`sha256sum $Firmware | cut -d ' ' -f1`
-echo -e "\n云端SHA256:$GET_SHA"
-echo -e "当前SHA256:$CURRENT_SHA\n"
-if [ $GET_SHA == "" ] || [ $CURRENT_SHA == "" ];then
-	echo "SHA256对比失败,请重新下载更新!"
+echo -e "\n当前SHA256:$CURRENT_SHA"
+echo -e "云端SHA256:$GET_SHA\n"
+if [ "$GET_SHA" == "" ] || [ "$CURRENT_SHA" == "" ];then
+	echo "SHA256获取失败!"
 	exit
-else
-	echo "SHA256对比通过,准备升级固件..."
 fi
+if [ ! "$GET_SHA" == "$CURRENT_SHA" ];then
+	echo "SHA256对比失败,请重试!"
+	exit
+fi
+echo "SHA256对比通过,准备升级固件..."
 sleep 3
 echo -e "\n开始升级固件,请耐心等待..."
 sysupgrade -v $Firmware
