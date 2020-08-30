4e6dfe89e0e45843dd0259d9fa4e900274358909
Hyy2001X <1804430051@qq.com>
2020-08-30 19:56:50 +0800

    V1.7

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -2,30 +2,31 @@
 # AutoBuild Script Module by Hyy2001
 # AutoUpdate
 
-Version=V1.6
+Version=V1.7
 Github=https://github.com/Hyy2001X/Openwrt-AutoUpdate
 Github_Release=$Github/releases/tag/AutoUpdate
 Github_Download=$Github/releases/download/AutoUpdate
+
 clear
-echo -e "Auto-Update Script $Version by Hyy2001\n"
 if [ ! -f /etc/openwrt_date ];then
-	echo "暂不兼容当前固件!"
+	echo "AutoUpdate 不兼容当前固件!"
 	exit
 fi
+echo -e "Auto-Update Script $Version by Hyy2001\n"
 CURRENT_VERSION=`cat /etc/openwrt_date`
 if [ "$CURRENT_VERSION" == "" ]; then
-	echo "当前固件信息获取失败!"
+	echo "固件信息获取失败!"
 	exit
 fi
 cd /tmp
 echo "开始获取云端版本信息..."
 GET_Version=`wget --no-check-certificate -q $Github_Release -O - | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+' | awk 'NR==1'`
 if [ "$GET_Version" == "" ]; then
-	echo "云端版本信息获取失败!"
+	echo -e "\n...获取失败!"
 	exit
 fi
-echo -e "\n当前版本: $CURRENT_VERSION"
-echo "云端版本: $GET_Version"
+echo -e "\n当前版本:$CURRENT_VERSION"
+echo "云端版本:$GET_Version"
 if [ $CURRENT_VERSION == $GET_Version ];then
 	echo -e "\n已是最新版本,无需更新!"
 	exit
@@ -33,7 +34,7 @@ fi
 Firmware=AutoBuild-d-team_newifi-d2-Lede-$GET_Version.bin
 Firmware_Detail=AutoBuild-d-team_newifi-d2-Lede-$GET_Version.detail
 echo "云端固件名称:$Firmware"
-echo -e "\n开始下载云端固件..."
+echo -e "\n正在下载云端固件..."
 wget --no-check-certificate -q $Github_Download/$Firmware -O $Firmware
 if [ ! "$?" == 0 ]; then
 	echo "[$Firmware]下载失败!"
@@ -48,19 +49,19 @@ if [ ! "$?" == 0 ]; then
 	exit
 fi
 echo "...下载成功!"
-GET_SHA=`awk -F'[ :]' '/SHA256/ {print $2;exit}' $Firmware_Detail`
-CURRENT_SHA=`sha256sum $Firmware | cut -d ' ' -f1`
-echo -e "\n当前SHA256:$CURRENT_SHA"
-echo -e "云端SHA256:$GET_SHA\n"
-if [ "$GET_SHA" == "" ] || [ "$CURRENT_SHA" == "" ];then
-	echo "SHA256获取失败!"
+GET_MD5=`awk -F'[ :]' '/MD5/ {print $2;exit}' $Firmware_Detail`
+CURRENT_MD5=`md5sum $Firmware | cut -d ' ' -f1`
+echo -e "\n当前文件MD5:$CURRENT_MD5"
+echo -e "云端文件MD5:$GET_MD5\n"
+if [ "$GET_MD5" == "" ] || [ "$CURRENT_MD5" == "" ];then
+	echo "MD5获取失败!"
 	exit
 fi
-if [ ! "$GET_SHA" == "$CURRENT_SHA" ];then
-	echo "SHA256对比失败,请重试!"
+if [ ! "$GET_MD5" == "$CURRENT_MD5" ];then
+	echo "MD5对比失败,请重试!"
 	exit
 fi
-echo "SHA256对比通过,准备升级固件..."
+echo "MD5对比通过,准备升级固件..."
 sleep 3
 echo -e "\n开始升级固件,请耐心等待..."
-sysupgrade -v $Firmware
+sysupgrade $Firmware
