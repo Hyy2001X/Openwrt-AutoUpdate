9046ca1424913755da04e20c6cf2f0eb5916d111
Hyy2001X <1804430051@qq.com>
2020-09-19 14:48:11 +0800

    Bump to V2.6-BETA

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -4,7 +4,7 @@
 # AutoUpdate
 
 Author=Hyy2001
-Version=V2.5-BETA
+Version=V2.6-BETA
 Updated=2020.09.19
 
 Github=https://github.com/Hyy2001X/AutoBuild-Actions-BETA
@@ -13,23 +13,20 @@ Github_Download=$Github/releases/download/AutoUpdate
 TARGET_PROFILE=d-team_newifi-d2
 
 clear
-if [ ! -f /etc/openwrt_date ] && [ ! -f /etc/openwrt_device ];then
-	echo "AutoUpdate 不兼容当前固件!"
-	exit
-fi
 echo -e "Auto-Update Script $Version by $Author\n"
-CURRENT_VERSION=`cat /etc/openwrt_date`
+cd /etc
+CURRENT_VERSION=`cat ./openwrt_date` > /dev/null 2>&1
 if [ "$CURRENT_VERSION" == "" ]; then
 	echo -e "警告:当前固件版本获取失败!\n"
 	CURRENT_VERSION=未知
 fi
-CURRENT_DEVICE=`cat /etc/openwrt_device`
+CURRENT_DEVICE=`cat ./openwrt_device` > /dev/null 2>&1
 if [ "$CURRENT_DEVICE" == "" ]; then
 	echo -e "警告:当前设备名称获取失败,使用预设设备名称[$TARGET_PROFILE]!\n"
 	CURRENT_DEVICE=$TARGET_PROFILE
 fi
 cd /tmp
-echo "正在获取云端固件版本信息..."
+echo "正在获取云端固件版本..."
 Check_Version=`wget --no-check-certificate -q $Github_Tags -O - | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+.bin' | awk 'NR==1'`
 if [ "$Check_Version" == "" ]; then
 	echo -e "\n...未获取到任何信息,请稍后重试!"
@@ -61,14 +58,11 @@ Firmware_Info=AutoBuild-$CURRENT_DEVICE-Lede-$GET_Version
 Firmware=${Firmware_Info}.bin
 Firmware_Detail=${Firmware_Info}.detail
 echo "云端固件名称:$Firmware"
-
-curl -I -s --connect-timeout 3 www.google.com -w %{http_code}
-
-echo -e "\n正在下载固件..."
 NETWORK=`curl -I -s --connect-timeout 5 www.google.com -w %{http_code} |  tail -n1`
-if [ ! "$NETWORK" == "200" ];then
+if [ ! "$NETWORK" == 200 ];then
 	echo -e "\nGoogle 连接失败,可能导致固件下载速度缓慢!"
 fi
+echo -e "\n正在下载固件,请耐心等待..."
 wget --no-check-certificate -q $Github_Download/$Firmware -O $Firmware
 if [ ! "$?" == 0 ]; then
 	echo "...下载失败,请检查网络后重试!"
