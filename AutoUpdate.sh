665b50e9aaf7ce106c486700c4cecf332a0b13f4
Hyy2001X <1804430051@qq.com>
2020-12-21 22:17:06 +0800

    Bump to V4.4

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
-# AutoUpdate
+# AutoUpdate for Openwrt
 
-Version=V4.1
+Version=V4.4
 DEFAULT_DEVICE=d-team_newifi-d2
 Github=https://github.com/Hyy2001X/AutoBuild-Actions
 
@@ -19,33 +19,58 @@ CURRENT_VERSION=$(awk 'NR==1' openwrt_info)
 CURRENT_DEVICE=$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')
 clear && echo "Openwrt-AutoUpdate Script ${Version}"
 if [[ -z "$1" ]];then
-	Upgrade_Options="-q" && TIME && echo "执行: 保留配置升级固件"
+	Upgrade_Options="-q" && TIME && echo "执行: 保留配置更新固件[静默模式]"
 else
-	Upgrade_Options="$1"
-	[[ "${Upgrade_Options}" == "-n" ]] && TIME && echo "执行: 不保留配置升级固件"
-	if [[ "${Upgrade_Options}" == "-x" ]];then
-		Upgrade_Options="-q"
+	case $1 in
+	-n)
+		TIME && echo "执行: 不保留配置更新固件"
+	;;
+	-q)
+		TIME && echo "执行: 保留配置更新固件[静默模式]"
+	;;
+	-v)
+		TIME && echo "执行: 保留配置更新固件[详细模式]"
+	;;
+	-f)
 		Force_Update="1"
-		TIME && echo "执行: 保留配置强制升级"
-	fi
+		Upgrade_Options="-q"
+		TIME && echo "执行: 强制更新固件并保留配置"
+	;;
+	*)
+		echo -e "\nUsage: bash /bin/AutoUpdate.sh [<Option>]"
+		echo -e "\n可使用的选项:"
+		echo "	-f	强制更新固件并保留配置"
+		echo "	-q	更新固件并保留配置[静默模式]"
+		echo "	-v	更新固件并保留配置[详细模式]"
+		echo "	-n	更新固件但不保留配置"
+		echo -e "\n项目地址: ${Github}"
+		echo -e "默认设备: ${DEFAULT_DEVICE}\n"
+		exit
+	;;
+	esac
+	[ ! $1 == "-f" ] && Upgrade_Options="$1"
 fi
 opkg list | awk '{print $1}' > /tmp/Package_list
-grep "curl" /tmp/Package_list > /dev/null 2>&1
-if [[ ! $? -ne 0 ]];then
-	Baidu_Check=$(curl -I -s --connect-timeout 5 www.baidu.com -w %{http_code} | tail -n1)
-	[ ! "$Baidu_Check" == 200 ] && TIME && echo "网络连接失败,请检查连接后重试!" && exit
-	Google_Check=$(curl -I -s --connect-timeout 5 www.google.com -w %{http_code} | tail -n1)
-	[ ! "$Google_Check" == 200 ] && TIME && echo "Google 连接失败,可能导致固件下载速度缓慢!"
+if [[ ! "${Force_Update}" == "1" ]];then
+	grep "curl" /tmp/Package_list > /dev/null 2>&1
+	if [[ ! $? -ne 0 ]];then
+		Google_Check=$(curl -I -s --connect-timeout 5 www.google.com -w %{http_code} | tail -n1)
+		[ ! "$Google_Check" == 200 ] && TIME && echo "Google 连接失败,可能导致固件下载速度缓慢!"
+	fi
 fi
 grep "wget" /tmp/Package_list > /dev/null 2>&1
 if [[ $? -ne 0 ]];then
-	TIME && read -p "未安装 wget,是否执行安装?[Y/n]:" Choose
-	if [ "${Choose}" == Y ] || [ "${Choose}" == y ];then
-		TIME && echo -e "开始安装 wget,请耐心等待...\n"
+	if [[ "${Force_Update}" == "1" ]];then
+		Choose="Y"
+	else
+		TIME && read -p "未安装[wget],是否执行安装?[Y/n]:" Choose
+	fi
+	if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
+		TIME && echo -e "开始安装[wget],请耐心等待...\n"
 		opkg update > /dev/null 2>&1
 		opkg install wget
 	else
-		TIME && echo "用户已取消安装,即将退出更新程序..."
+		TIME && echo "用户已取消安装,即将退出更新脚本..."
 		sleep 2
 		exit
 	fi
@@ -55,6 +80,7 @@ if [[ -z "${CURRENT_VERSION}" ]];then
 	CURRENT_VERSION="未知"
 fi
 if [[ -z "${CURRENT_DEVICE}" ]];then
+	[[ "${Upgrade_Options}" == "-x" ]] && exit
 	TIME && echo "警告: 当前设备名称获取失败,使用预设名称[$DEFAULT_DEVICE]"
 	CURRENT_DEVICE="${DEFAULT_DEVICE}"
 fi
@@ -72,12 +98,12 @@ echo "设备名称: ${DEFAULT_DEVICE}"
 echo -e "\n当前固件版本: ${CURRENT_VERSION}"
 echo -e "云端固件版本: ${GET_Version}"
 if [[ ! ${Force_Update} == 1 ]];then
-	if [ "${CURRENT_VERSION}" == "${GET_Version}" ];then
+	if [[ "${CURRENT_VERSION}" == "${GET_Version}" ]];then
 		TIME && read -p "已是最新版本,是否强制更新固件?[Y/n]:" Choose
-		if [ "${Choose}" == Y ] || [ "${Choose}" == y ];then
+		if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
 			TIME && echo -e "开始强制更新固件...\n"
 		else
-			TIME && echo "用户已取消强制更新,即将退出更新程序..."
+			TIME && echo "已取消强制更新,即将退出更新程序..."
 			sleep 2
 			exit
 		fi
@@ -91,28 +117,30 @@ cd /tmp
 TIME && echo "正在下载固件,请耐心等待..."
 wget -q ${Github_Download}/${Firmware} -O ${Firmware}
 if [[ ! $? == 0 ]];then
-	TIME && echo "下载失败,请检查网络后重试!"
+	TIME && echo "固件下载失败,请检查网络后重试!"
 	exit
 fi
 TIME && echo "下载成功!固件大小:$(du -h ${Firmware} | awk '{print $1}')B"
-TIME && echo "正在下载固件详细信息..."
+TIME && echo "正在获取云端固件MD5,请耐心等待..."
 wget -q ${Github_Download}/${Firmware_Detail} -O ${Firmware_Detail}
 if [[ ! $? == 0 ]];then
-	TIME && echo "下载失败,请检查网络后重试!"
+	TIME && echo "MD5 获取失败,请检查网络后重试!"
 	exit
 fi
-GET_MD5=$(awk -F'[ :]' '/MD5/ {print $2;exit}' ${Firmware_Detail})
+GET_MD5=$(awk -F '[ :]' '/MD5/ {print $2;exit}' ${Firmware_Detail})
 CURRENT_MD5=$(md5sum ${Firmware} | cut -d ' ' -f1)
-echo -e "\n当前固件MD5:${CURRENT_MD5}"
+echo -e "\n本地固件MD5:${CURRENT_MD5}"
 echo "云端固件MD5:${GET_MD5}"
 if [[ -z "${GET_MD5}" ]] || [[ -z "${CURRENT_MD5}" ]];then
-	echo -e "\nMD5获取失败!"
+	echo -e "\nMD5 获取失败!"
 	exit
 fi
 if [[ ! "${GET_MD5}" == "${CURRENT_MD5}" ]];then
-	echo -e "\nMD5对比失败,请检查网络后重试!"
+	echo -e "\nMD5 对比失败,请检查网络后重试!"
 	exit
+else
+	TIME && echo -e "MD5 对比通过!"
 fi
-TIME && echo -e "开始升级固件,请耐心等待...\n"
+TIME && echo -e "开始更新固件,请耐心等待路由器重启...\n"
 sleep 3
 sysupgrade ${Upgrade_Options} ${Firmware}
